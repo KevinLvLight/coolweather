5421daa83122ae12f1b058a88439957b79c80b9f
Lcy <2786848117@qq.com>
2017-11-19 09:00:32 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/android/coolweather/com/coolweather/db/Province.java
@@ -0,0 +1,39 @@
+package android.coolweather.com.coolweather.db;
+
+/**
+ * Created by LV on 2017/11/18.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public  int getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+
+}
